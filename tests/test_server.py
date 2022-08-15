ef3d43afca818978875429d9400fab034631b2b9
Michael Morgan <git@morgan83.com>
2022-08-15 16:33:45 -0400

    Add rtt_ms integration test

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,10 +1,12 @@
 """Server interceptor tests."""
 import logging
+import re
 from unittest.mock import Mock
 
 from pytest import LogCaptureFixture
 
 from grpc_accesslog import AccessLogInterceptor
+from grpc_accesslog import handlers
 
 
 def test_default_handlers() -> None:
@@ -31,3 +33,21 @@ def test_intercept(caplog: LogCaptureFixture) -> None:
     interceptor.intercept(Mock(), Mock(), Mock(), "/abc/Test")
 
     assert "this that" in caplog.text
+
+
+def test_rtt_handler_succeeds(caplog: LogCaptureFixture) -> None:
+    """Test RTT handler should not raise errors through interceptor."""
+    caplog.set_level(logging.INFO, logger="root")
+
+    interceptor = AccessLogInterceptor(
+        name="root",
+        handlers=(
+            lambda _: "this",
+            handlers.rtt_ms,
+        ),
+        propagate=True,
+    )
+
+    interceptor.intercept(Mock(), Mock(), Mock(), "/abc/Test")
+
+    assert re.match(r"^.*this \d+", caplog.text)
