1046dcefafe6f354d85cd7ddb9f7a4f6e27f0a57
Michael Morgan <git@morgan83.com>
2023-07-19 20:26:08 -0400

    Start new tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,14 +1,20 @@
 """Server interceptor tests."""
 import logging
 import re
+from concurrent import futures
 from typing import Iterator
 from unittest.mock import Mock
 
+import grpc
+import pytest
 from pytest import LogCaptureFixture
 
 from grpc_accesslog import AccessLogInterceptor
 from grpc_accesslog import handlers
 from grpc_accesslog._context import LogContext
+
+from .proto import test_service_pb2
+from .proto import test_service_pb2_grpc
 
 
 def test_default_handlers() -> None:
@@ -32,7 +38,16 @@ def test_intercept(caplog: LogCaptureFixture) -> None:
         propagate=True,
     )
 
-    interceptor.intercept(Mock(), Mock(), Mock(), "/abc/Test")
+    interceptor.intercept_service(
+        Mock(
+            return_value=Mock(
+                name="RpcMethodHandler",
+                request_streaming=False,
+                response_streaming=False,
+            )
+        ),
+        Mock(name="HandlerCallDetails"),
+    )
 
     assert "this that" in caplog.text
 
@@ -106,3 +121,36 @@ def test_rtt_handler_succeeds(caplog: LogCaptureFixture) -> None:
     interceptor.intercept(Mock(), Mock(), Mock(), "/abc/Test")
 
     assert re.match(r"^.*this \d+", caplog.text)
+
+
+def test_testerson(caplog):
+    """Testing the testersons."""
+    caplog.set_level(logging.INFO, logger="root")
+
+    interceptor = AccessLogInterceptor(
+        name="root",
+        handlers=(
+            lambda _: "this",
+            lambda _: "that",
+        ),
+        propagate=True,
+    )
+
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=1), interceptors=[interceptor]
+    )
+    port = server.add_insecure_port("localhost:0")
+    servicer = test_service_pb2_grpc.TestServiceServicer()
+
+    test_service_pb2_grpc.add_TestServiceServicer_to_server(servicer, server)
+
+    server.start()
+
+    with grpc.insecure_channel(f"localhost:{port}") as channel:
+        stub = test_service_pb2_grpc.TestServiceStub(channel)
+        with pytest.raises(grpc.RpcError, match="UNIMPLEMENTED"):
+            stub.UnaryUnary(test_service_pb2.Request())
+
+    server.stop(grace=0)
+
+    assert "this that" in caplog.text
