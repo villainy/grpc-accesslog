c634766eaf9d5eb1a5c88b324ae4789c04e9aad3
Michael Morgan <git@morgan83.com>
2023-07-22 10:43:18 -0400

    Exclude generate code from mypy

---
+++ tests/proto/test_service_pb2.pyi
@@ -0,0 +1,20 @@
+from typing import ClassVar as _ClassVar
+from typing import Optional as _Optional
+
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Request(_message.Message):
+    __slots__ = ["data"]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    data: str
+    def __init__(self, data: _Optional[str] = ...) -> None: ...
+
+class Response(_message.Message):
+    __slots__ = ["data"]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    data: str
+    def __init__(self, data: _Optional[str] = ...) -> None: ...
