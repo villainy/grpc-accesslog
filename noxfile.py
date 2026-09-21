fff612822144c4e7fe552389845e402173bb3e41
Michael Morgan <Michael Morgan>
2026-09-21 15:03:01 -0400

    Fix noxfile sessions typing

--- noxfile.py
+++ noxfile.py
@@ -25,14 +25,14 @@ except ImportError:
 package = "grpc_accesslog"
 python_versions = ["3.13", "3.12", "3.11", "3.10"]
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = (
+nox.options.sessions = [
     "pre-commit",
     "safety",
     "mypy",
     "tests",
     "xdoctest",
     "docs-build",
-)
+]
 
 
 def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
